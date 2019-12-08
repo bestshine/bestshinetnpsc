7a5013ca44072397fa066cb533072552c3f14068
example1 <32572574+bestshine@users.noreply.github.com>
2019-12-08 11:17:24 +0530

    website modi
    website modi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,16 +64,16 @@
                     <div class="carousel-inner-content col-md-6">
                         <div class="row">
                             <div class="banner-carousel-cont text-center">
-                               <!-- <span>
+                                <span>
                                     Smart way to prepare for <span class="yellow">Online Test</span>
                                 </span>
                                 <div class="col-md-12">
                                     <a href="#" class="get-courses-btn dilb">Get courses</a>
-                                </div>-->
-                               <!-- <div class="col-md-12 Anouncement-wr">
+                                </div>
+                               <div class="col-md-12 Anouncement-wr">
                                     <label>Anouncement</label>
                                     <div class="Anouncement-marquee"><marquee>Smart way to prepare for NEET Smart way to prepare for NEET</marquee></div>
-                                </div>-->
+                                </div>
                             </div>
                         </div>
                     </div>
